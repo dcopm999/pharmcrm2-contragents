4ffe87c9254f729aa67db5ad251f15cc4d465c02
master <dcopm999@gmail.com>
2022-07-10 16:42:45 +0500

    Adding models
    * contragents/models.py: Added Contragent and contact models
    * contragents/admin.py: Added ne models to admin panel
    * contragents/migrations/0001_initial.py: Added migrations
    * requirements.txt: Updated
    * tests/settings.py: Added hr and address to INSTALLED_APPS

--- contragents/admin.py
+++ contragents/admin.py
@@ -1,1 +1,60 @@
 # -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from contragents import models
+
+
+class ContactInline(admin.StackedInline):
+    model = models.Contact
+    suit_classes = "suit-tab suit-tab-contact"
+    extra = 1
+
+
+@admin.register(models.Contragent)
+class ContragentAdmin(admin.ModelAdmin):
+    inlines = [ContactInline]
+    suit_form_tabs = (
+        ("general", "General"),
+        ("address", "Address"),
+        ("contact", "Contact"),
+    )
+
+    fieldsets = [
+        (
+            "General",
+            {
+                "classes": (
+                    "suit-tab",
+                    "suit-tab-general",
+                ),
+                "fields": ["contragent_type", "name", "manager"],
+            },
+        ),
+        (
+            None,
+            {
+                "classes": (
+                    "suit-tab",
+                    "suit-tab-general",
+                ),
+                "fields": ["inn", "okonh", "okpo", "oked"],
+            },
+        ),
+        (
+            "Address",
+            {
+                "classes": (
+                    "suit-tab",
+                    "suit-tab-address",
+                ),
+                "fields": [
+                    "country",
+                    "region",
+                    "city",
+                    "district",
+                    "street",
+                    "address_ur",
+                ],
+            },
+        ),
+    ]
